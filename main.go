07c71f243f8bd263f1f3d89f2e6cc0bf575a21af
pstet <123316024+pcs03@users.noreply.github.com>
2024-06-23 16:37:00 +0200

    Add a REPL

--- main.go
+++ main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+    "os"
+    "os/user"
+    "netherlang/repl"
 )
 
 func main() {
-    fmt.Println()
+    user, err := user.Current()
+    if err != nil {
+        panic(err)
+    }
 
+    fmt.Printf("Welkom %s! Dit is de NetherLang Programeertaal!\n", user.Username)
+    repl.Start(os.Stdin, os.Stdout)
 }
