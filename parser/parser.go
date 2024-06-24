c04381d1bc3e450dd71f45b5fca6a612195d6e44
pstet <123316024+pcs03@users.noreply.github.com>
2024-06-24 02:15:23 +0200

    Start on return statement parser

--- parser/parser.go
+++ parser/parser.go
@@ -61,6 +61,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+    case token.RETURN:
+        return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -85,6 +87,8 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
     return statement
 }
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {}
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
     return p.curToken.Type == t
